7f98294598a825d26ccc1754020a7582fff6f6ac
ataksale <34750862+ataksale@users.noreply.github.com>
2026-06-29 14:38:33 +0530

    Create 01_Create_Tables.sql
    New Section added

---
+++ sql/01_Create_Tables.sql
@@ -0,0 +1,63 @@
+/*
+====================================================
+Project : Payment Settlement Reconciliation Dashboard
+Author  : Your Name
+Purpose : Create database tables
+====================================================
+*/
+
+CREATE TABLE Merchants
+(
+    MerchantID VARCHAR(20) PRIMARY KEY,
+    MerchantName VARCHAR(100),
+    Country VARCHAR(50),
+    SettlementCycle VARCHAR(10),
+    PSP VARCHAR(100),
+    Acquirer VARCHAR(100),
+    Status VARCHAR(20)
+);
+
+CREATE TABLE Transactions
+(
+    TransactionID VARCHAR(30) PRIMARY KEY,
+    MerchantID VARCHAR(20),
+    Network VARCHAR(30),
+    CardType VARCHAR(30),
+    BIN VARCHAR(10),
+    Amount DECIMAL(18,2),
+    Currency VARCHAR(10),
+    TransactionDate DATETIME,
+    AuthorizationCode VARCHAR(20),
+    ResponseCode VARCHAR(5),
+    Status VARCHAR(20)
+);
+
+CREATE TABLE Settlement
+(
+    SettlementID VARCHAR(30) PRIMARY KEY,
+    TransactionID VARCHAR(30),
+    SettlementDate DATE,
+    SettlementAmount DECIMAL(18,2),
+    SettlementCurrency VARCHAR(10),
+    SettlementStatus VARCHAR(20),
+    SettlementCycle VARCHAR(10)
+);
+
+CREATE TABLE Fraud
+(
+    FraudID VARCHAR(30),
+    TransactionID VARCHAR(30),
+    RiskScore INT,
+    FraudType VARCHAR(50),
+    Status VARCHAR(20)
+);
+
+CREATE TABLE Chargeback
+(
+    ChargebackID VARCHAR(30),
+    TransactionID VARCHAR(30),
+    ReasonCode VARCHAR(20),
+    Amount DECIMAL(18,2),
+    ChargebackDate DATE,
+    Status VARCHAR(20)
+);
